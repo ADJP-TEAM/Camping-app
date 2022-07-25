d18bf9aab073bcdef94115abe7e6b997a2ea0839
JustinWouters <justinwouters@gmail.com>
2022-07-25 13:58:47 -0700

    made changes to homepage

--- client/App.tsx
+++ client/App.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import './stylesheets/styles.scss';
 
 export default function App() {
-  return <div>Hello from AdventuRent app!</div>;
+  return <div>Hello from AdventuRent application.</div>;
 }
